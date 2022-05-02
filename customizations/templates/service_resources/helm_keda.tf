67804165dff7db99ce5939d10907e4c98ca9e010
Maninder Cheema <Maninder.Cheema@halliburton.com>
2022-05-02 13:27:41 +0000

    Enabling  AGIC and KEDA Back and updating KEDA version to see if it Deploys

--- customizations/templates/service_resources/helm_keda.tf
+++ customizations/templates/service_resources/helm_keda.tf
@@ -6,30 +6,30 @@
 # */
 
 
-# #-------------------------------
-# # Keda
-# #-------------------------------
-# locals {
-#   helm_keda_name    = "keda"
-#   helm_keda_ns      = "keda"
-#   helm_keda_repo    = "https://kedacore.github.io/charts"
-#   helm_keda_version = var.keda_v2_enabled == true ? "2.2.0" : "1.5.0"
-# }
+#-------------------------------
+# Keda
+#-------------------------------
+locals {
+  helm_keda_name    = "keda"
+  helm_keda_ns      = "keda"
+  helm_keda_repo    = "https://kedacore.github.io/charts"
+  helm_keda_version = var.keda_v2_enabled == true ? "2.3.2" : "2.2.0"
+}
 
-# resource "kubernetes_namespace" "keda" {
-#   metadata {
-#     name = local.helm_keda_ns
-#   }
+resource "kubernetes_namespace" "keda" {
+  metadata {
+    name = local.helm_keda_ns
+  }
 
-#   depends_on = [module.aks]
-# }
+  depends_on = [module.aks]
+}
 
-# resource "helm_release" "keda" {
-#   name       = local.helm_keda_name
-#   repository = local.helm_keda_repo
-#   chart      = "keda"
-#   version    = local.helm_keda_version
-#   namespace  = local.helm_keda_ns
+resource "helm_release" "keda" {
+  name       = local.helm_keda_name
+  repository = local.helm_keda_repo
+  chart      = "keda"
+  version    = local.helm_keda_version
+  namespace  = local.helm_keda_ns
 
-#   depends_on = [kubernetes_namespace.keda, azurerm_role_assignment.osdu_identity_mi_operator]
-# }
+  depends_on = [kubernetes_namespace.keda, azurerm_role_assignment.osdu_identity_mi_operator]
+}
